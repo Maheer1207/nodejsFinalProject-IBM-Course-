86973b540bbaddb82c1679b941adcfb610a3c8cf
Maheer1207 <maheer.1207@gmail.com>
2023-01-26 23:28:58 -0500

    codes for 10, 11, 12, 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,25 +20,52 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({message: "Unable to register user"});
 });
 
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify(books, null, 4));
+  let myPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(res.send(JSON.stringify(books, null, 4)))
+    },6000)})
+
+  //Call the promise and wait for it to be resolved and then print a message.
+  myPromise.then((success) => {
+    console.log("From Callback: " + success)
+  })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   var isbn = req.params.isbn;
-  return res.send(books[isbn]);
+
+  let myPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(res.send(books[isbn]))
+    },6000)})
+
+  //Call the promise and wait for it to be resolved and then print a message.
+  myPromise.then((success) => {
+    console.log("From Callback: " + success)
+  })
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+  
   var itr = Object.keys(books);
   var auth = req.params.author;
 
   for (var key of itr) {
     if(books[key].author === auth) {
-      return res.send(books[key]);
+      let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve(res.send(books[key])          )
+        },6000)})
+        
+      //Call the promise and wait for it to be resolved and then print a message.
+      myPromise.then((success) => {
+        console.log("From Callback: " + success)
+      })
     }
   }
 });
@@ -50,7 +77,15 @@ public_users.get('/title/:title',function (req, res) {
 
   for (var key of itr) {
     if(books[key].title === title) {
-      return res.send(books[key]);
+      let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve(res.send(books[key])          )
+        },6000)})
+        
+      //Call the promise and wait for it to be resolved and then print a message.
+      myPromise.then((success) => {
+        console.log("From Callback: " + success)
+      })
     }
   }
 });
